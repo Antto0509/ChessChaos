0b17edb9e90e5dcd5ddf5b42d40016e6803e7e51
Antoine Coutreel <114882531+Antto0509@users.noreply.github.com>
2024-10-10 17:04:02 +0200

    Add Audio

--- Assets/Script/AudioController.cs
+++ Assets/Script/AudioController.cs
@@ -1,59 +1,22 @@
-using System;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class AudioController : MonoBehaviour
 {
-    public AudioClip clip; // L'audio clip à jouer
-    
-    [Range(0f, 1f)]
-    public float volume; // Le volume de l'audio clip 
-    
-    [Range(0.1f, 2.5f)]
-    public float pitch; // Le pitch de l'audio clip
-    
-    private AudioSource _source; // La source audio
+    public AudioMixer audioMixer;
 
-    private void Awake()
+    public void SetMasterVolume(float volume)
     {
-        gameObject.AddComponent<AudioSource>(); // Ajoute un composant AudioSource à l'objet
-        _source = GetComponent<AudioSource>(); // Récupère le composant AudioSource
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
 
-        volume = 0.5f;
-        pitch = 1f;
-    }
-    
-    // Méthode pour jouer l'audio clip
-    public void Start()
+    public void SetMusicVolume(float volume)
     {
-        _source.clip = clip; // Attribue l'audio clip à la source audio
-        _source.volume = volume; // Attribue le volume à la source audio
-        _source.pitch = pitch; // Attribue le pitch à la source audio
+        audioMixer.SetFloat("MusicVolume", volume);
     }
-    
-    // Méthode pour mettre en pause l'audio clip
-    public void Update()
+
+    public void SetSFXVolume(float volume)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PayAndPause();
-        }
-        
-        _source.volume = volume; // Attribue le volume à la source audio
-        _source.pitch = pitch; // Attribue le pitch à la source audio
-    }
-    
-    // Méthode pour jouer ou mettre en pause l'audio clip
-    public void PayAndPause()
-    {
-        if (_source.isPlaying)
-        {
-            _source.Pause();
-        }
-        else
-        {
-            _source.Play();
-        }
+        audioMixer.SetFloat("SFXVolume", volume);
     }
 }
