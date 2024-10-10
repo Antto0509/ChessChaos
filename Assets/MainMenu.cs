867473e128e4c49cb373f45e50f4705c2975f88f
Spiderus-git <maxence.leporcq@etud.u-picardie.fr>
2024-10-10 15:20:43 +0200

    Bouton Jouer fonctionnel, Bouton quitter également

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public string AIdifficulty;
     public void StartGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(AIdifficulty);
     }
 
     public void SettingsButton()
