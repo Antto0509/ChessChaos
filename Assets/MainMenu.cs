11c29f8bf480977a404d0360335bd5ceff940908
Spiderus-hub <maxence.leporcq@etud.u-picardie.fr>
2024-10-10 14:25:05 +0200

    Ajout du MainMenu (Interface non fonctionnelle et d'un début de script pour la navigation

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SettingsButton()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
