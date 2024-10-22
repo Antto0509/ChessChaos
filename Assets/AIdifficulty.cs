c5189d300a2b621bd9a1c6bcfddcac9cba28ed20
Antoine Coutreel <114882531+Antto0509@users.noreply.github.com>
2024-10-22 15:32:42 +0200

    Update AIdifficulty

--- Assets/AIdifficulty.cs
+++ Assets/AIdifficulty.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AIdifficulty : MonoBehaviour
 {
+    private Process stockfish;
     public string RetourMainMenu;
     public string Chessboard;
     public int Difficulty;
@@ -37,10 +39,67 @@ public class AIdifficulty : MonoBehaviour
         AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
     }
     
+    /* void StartStockfish()
+    {
+        string stockfishPath = GetStockfishPath();
+
+        if (!string.IsNullOrEmpty(stockfishPath))
+        {
+            stockfish = new Process();
+            stockfish.StartInfo.FileName = stockfishPath;
+            stockfish.StartInfo.UseShellExecute = false;
+            stockfish.StartInfo.RedirectStandardInput = true;
+            stockfish.StartInfo.RedirectStandardOutput = true;
+            stockfish.StartInfo.CreateNoWindow = true;
+            stockfish.Start();
+        }
+        else
+        {
+            Debug.LogError("Impossible de localiser Stockfish pour cet OS.");
+        }
+    }
+    
+    string GetStockfishPath()
+    {
+        string path = string.Empty;
+
+        // Vérifie l'OS sur lequel Unity est exécuté
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            path = Application.dataPath + "/Stockfish/Windows/stockfish-windows-x86-64-avx2.exe";
+        }
+        else if (Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXEditor)
+        {
+            path = Application.dataPath + "/Stockfish/macOS/stockfish-macos-m1-apple-silicon";
+        }
+
+        return path;
+    }
+
+    void SetSkillLevel(int level)
+    {
+        if (stockfish != null && stockfish.HasExited == false)
+        {
+            stockfish.StandardInput.WriteLine($"setoption name Skill Level value {level}");
+            stockfish.StandardInput.Flush();
+        }
+    } */
+    
     public void StartGame()
     {
-        PlayerPrefs.SetInt("Difficulty", Difficulty);
+        // StartStockfish();
+        // SetSkillLevel(Difficulty);
         SceneManager.LoadScene(Chessboard);
         AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
     }
+
+    /* void OnApplicationQuit()
+    {
+        if (stockfish != null && stockfish.HasExited == false)
+        {
+            stockfish.StandardInput.WriteLine("quit");
+            stockfish.StandardInput.Flush();
+            stockfish.Close();
+        }
+    } */
 }
