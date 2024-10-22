5e1efcda7d394f14a8612992de5efccd0b8f040a
Antoine Coutreel <114882531+Antto0509@users.noreply.github.com>
2024-10-22 15:10:41 +0200

    Update AIdifficulty : Add difficulty level

--- Assets/AIdifficulty.cs
+++ Assets/AIdifficulty.cs
@@ -5,15 +5,41 @@ public class AIdifficulty : MonoBehaviour
 {
     public string RetourMainMenu;
     public string Chessboard;
+    public int Difficulty;
     
     public void Retour()
     {
         SceneManager.LoadScene(RetourMainMenu);
         AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
     }
+
+    public void Facile()
+    {
+        Difficulty = 5;
+        AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
+    }
+    
+    public void Moyen()
+    {
+        Difficulty = 10;
+        AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
+    }
+    
+    public void Difficile()
+    {
+        Difficulty = 15;
+        AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
+    }
+    
+    public void Expert()
+    {
+        Difficulty = 20;
+        AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
+    }
     
     public void StartGame()
     {
+        PlayerPrefs.SetInt("Difficulty", Difficulty);
         SceneManager.LoadScene(Chessboard);
         AudioManager.Instance.PlaySound(AudioType.ClickSound, AudioSourceType.Player);
     }
